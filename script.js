e527ce854c46c633a8aca08510d52db790132d30
maniraad <maniraad7@gmail.com>
2023-11-05 21:26:51 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// let bodyColors = [
+//   "#a8d530",
+//   "#42aaff",
+//   "#f3d55b",
+//   "#ff4f51",
+//   "#904ae8",
+//   "#ffa711",
+// ];
+
+const $ =document;
+const faceBtn = $.querySelector('.face-btn')
+const colorBtn = $.querySelector('.color-btn')
+const hornsBtn = $.querySelector('.horns-btn')
+const tailBtn = $.querySelector('.tail-btn')
